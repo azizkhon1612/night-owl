8873823c1f45758b3d58f88a2bd1f560fc3cda15
MAMATOV AZIZKHON <mamatov717@gmail.com>
2022-09-29 12:07:57 +0900

    Deletion

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import "react-circular-progressbar/dist/styles.css";
 import "react-toastify/dist/ReactToastify.css";
 
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
@@ -19,12 +19,3 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(
-  <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </QueryClientProvider>
-  </BrowserRouter>
-);
