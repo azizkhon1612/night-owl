73d2174dcb10ef91d69b8b9c228cbd13a32c27ad
DevAtShafiq <98973818+DevAtShafiq@users.noreply.github.com>
2022-10-06 12:16:32 +0900

    aa
    df

--- src/pages/Movie/MovieInfo.tsx
+++ src/pages/Movie/MovieInfo.tsx
@@ -1,10 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { FC } from "react";
 import { useParams } from "react-router-dom";
-import FilmDetail from "../../components/FilmDetail/FilmDetail";
+import FilmDetail from "../../components/FilmDetail";
 import { getMovieFullDetail } from "../../services/movie";
 import { FilmInfo } from "../../shared/types";
-import Error from "../Error";
+//import Error from "../Error";
 
 const MovieInfo: FC = () => {
   const { id } = useParams();
