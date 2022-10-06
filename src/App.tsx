f9626cf838f74c62071173b654654b7058ea187b
MAMATOV AZIZKHON <mamatov717@gmail.com>
2022-10-06 16:17:25 +0900

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-
-import React from "react";
-
-import { Route, Routes } from "react-router-dom";
-
+import { useEffect, useState } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 
 import Home from "./pages/Home";
+import MovieInfo from "./pages/Movie/MovieInfo";
+
 
 
 function App() {
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname, location.search]);
+
   return (
     <Routes>
       <Route index element={<Home />} />
-   
+      <Route path="movie/:id" element={<MovieInfo />} />
+
     </Routes>
   );
 }
--- src/shared/const.ts
+++ src/shared/const.ts
@@ -1,5 +1,7 @@
 export const Api_Url="https://api.themoviedb.org/3"
-export const Image_Url="https://image.tmdb.org/t/p"
+export const IMAGE_URL = "https://image.tmdb.org/t/p";
+export const EMBED_URL = "https://2embed.org/embed";
+export const EMBED_TO = "https://www.2embed.to/embed/tmdb";
 
 export const Supported_Query={
     genre:[],
