ae43ded886a33e54b14f3b1b4f67d19ed705e7d3
MAMATOV AZIZKHON <mamatov717@gmail.com>
2022-10-12 12:18:30 +0900

    Final fixes
    homepage with complete film and tv info

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import { Route, Routes } from "react-router-dom";
 
 
 import Home from "./pages/Home";
-
+import MovieInfo from "./pages/Movie/MovieInfo";
+import TVInfo from "./pages/TV/TVInfo";
+import Error from "./pages/Error";
 
 function App() {
   return (
     <Routes>
       <Route index element={<Home />} />
+      <Route path="movie/:id" element={<MovieInfo />} />
+      <Route path="tv/:id" element={<TVInfo />} />
+
     </Routes>
   );
 }
