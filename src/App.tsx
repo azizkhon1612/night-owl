2a345360c06d55e3876a82275d7d136b1accc160
MAMATOV AZIZKHON <mamatov717@gmail.com>
2022-10-12 09:49:50 +0900

    Fixed errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 
+
+import Error from "./pages/Error";
+import Explore from "./pages/Explore";
+
 import Home from "./pages/Home";
 import MovieInfo from "./pages/Movie/MovieInfo";
 
+import TVInfo from "./pages/TV/TVInfo";
 
 
 function App() {
@@ -14,9 +19,11 @@ function App() {
 
   return (
     <Routes>
-      <Route index element={<Home />} />
+     <Route index element={<Home />} />
       <Route path="movie/:id" element={<MovieInfo />} />
-
+      <Route path="tv/:id" element={<TVInfo />} />
+      <Route path="explore" element={<Explore />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -10,9 +10,11 @@ import "react-toastify/dist/ReactToastify.css";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
-import React from "react";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -20,10 +22,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <BrowserRouter>
-    
+    <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <App />
       </Provider>
-  
+    </QueryClientProvider>
   </BrowserRouter>
 );
