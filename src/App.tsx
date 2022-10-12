0eee4af23f7ac7da431e591f0f15b75f680c9a39
MAMATOV AZIZKHON <mamatov717@gmail.com>
2022-10-12 12:06:11 +0900

    Update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 
 
-import Error from "./pages/Error";
-import Explore from "./pages/Explore";
-import Auth from "./pages/Auth";
-import Home from "./pages/Home";
-import MovieInfo from "./pages/Movie/MovieInfo";
-import Search from "./pages/Search";
 
-import TVInfo from "./pages/TV/TVInfo";
+import Home from "./pages/Home";
 
 
 function App() {
   return (
     <Routes>
       <Route index element={<Home />} />
-      <Route path="movie/:id" element={<MovieInfo />} />
-      <Route path="tv/:id" element={<TVInfo />} />
-
-      <Route path="explore" element={<Explore />} />
-      <Route path="search" element={<Search />} />
-      <Route path="auth" element={<Auth />} />
-      
-      <Route path="*" element={<Error />} />
     </Routes>
   );
 }
