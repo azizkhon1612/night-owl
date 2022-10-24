3513325030a715c907f4ab4002e4a690b1ba0012
MAMATOV AZIZKHON <mamatov717@gmail.com>
2022-10-24 10:33:12 +0900

    Routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
-import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 
 
-
+import Error from "./pages/Error";
 import Home from "./pages/Home";
 import MovieInfo from "./pages/Movie/MovieInfo";
 import TVInfo from "./pages/TV/TVInfo";
+import TVWatch from "./pages/TV/TVWatch";
+
+
 
 function App() {
+  const location = useLocation();
+  // const currentUser = useAppSelector((state) => state.auth.user);
+
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname, location.search]);
+
   return (
     <Routes>
       <Route index element={<Home />} />
       <Route path="movie/:id" element={<MovieInfo />} />
       <Route path="tv/:id" element={<TVInfo />} />
-
+      
+      <Route path="tv/:id/watch" element={<TVWatch />} />
+     
+       <Route path="*" element={<Error />} />
     </Routes>
   );
 }
