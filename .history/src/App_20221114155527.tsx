17c407e73eaa3331f0ed3617426a54d6d0a3a4f6
Hieu <hieunguyen250298@gmail.com>
2022-11-16 10:38:30 +0900

    fixed

---
+++ .history/src/App_20221114155527.tsx
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Search from "./pages/Search";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import MovieInfo from "./pages/Movie/MovieInfo";
+import TVInfo from "./pages/TV/TVInfo";
+import TVWatch from "./pages/TV/TVWatch";
+import MovieWatch from "./pages/Movie/MovieWatch";
+import Explore from "./pages/Explore";
+
+function App() {
+  const location = useLocation();
+  // const currentUser = useAppSelector((state) => state.auth.user);
+
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname, location.search]);
+
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="movie/:id" element={<MovieInfo />} />
+      <Route path="tv/:id" element={<TVInfo />} />
+      <Route path="movie/:id/watch" element={<MovieWatch />} />
+      <Route path="tv/:id/watch" element={<TVWatch />} />
+      <Route path="explore" element={<Explore />} />
+      <Route path="search" element={<Search />} />
+      <Route
+        path="profile"
+        element={
+          <Protected isSignedIn={isSignedIn}>
+            <Profile />
+          </Protected>
+        }
+      />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  );
+}
+
+export default App;
--- .history/src/App_20221116103728.tsx
+++ .history/src/App_20221116103728.tsx
@@ -1,11 +1,24 @@
+// 
+
 import { onAuthStateChanged } from "firebase/auth";
 import { doc, onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 
-
+import Protected from "./components/Common/Protected";
+import Error from "./pages/Error";
+import Explore from "./pages/Explore";
+import History from "./pages/History";
 import Home from "./pages/Home";
-
+import MovieInfo from "./pages/Movie/MovieInfo";
+import MovieWatch from "./pages/Movie/MovieWatch";
+import Profile from "./pages/Profile";
+import Search from "./pages/Search";
+import TVInfo from "./pages/TV/TVInfo";
+import TVWatch from "./pages/TV/TVWatch";
+import { auth, db } from "./shared/firebase";
+import { useAppDispatch } from "./store/hooks";
+import { setCurrentUser } from "./store/slice/authSlice";
 
 function App() {
   const location = useLocation();
@@ -79,6 +92,26 @@ function App() {
   return (
     <Routes>
       <Route index element={<Home />} />
+      <Route path="movie/:id" element={<MovieInfo />} />
+      <Route path="tv/:id" element={<TVInfo />} />
+      <Route path="movie/:id/watch" element={<MovieWatch />} />
+      <Route path="tv/:id/watch" element={<TVWatch />} />
+      <Route path="explore" element={<Explore />} />
+      <Route path="search" element={<Search />} />
+      
+      <Route
+        path="history"
+        element={<Protected isSignedIn={isSignedIn}>{<History />}</Protected>}
+      />
+      <Route
+        path="profile"
+        element={
+          <Protected isSignedIn={isSignedIn}>
+            <Profile />
+          </Protected>
+        }
+      />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 }
