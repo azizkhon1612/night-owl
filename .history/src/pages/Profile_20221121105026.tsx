93a721f881e87975cd11714ec2738115f12b0b29
Hieu <hieunguyen250298@gmail.com>
2022-11-24 12:54:49 +0900

    AAA

--- .history/src/pages/Profile_20221121105026.tsx
+++ .history/src/pages/Profile_20221121105026.tsx
@@ -176,7 +176,7 @@ const Profile: FunctionComponent<ProfileProps> = () => {
 
   return (
     <>
-      <Title value="Profile | NightOwl" />
+      <Title value="Profile | Moonlight" />
 
       <ToastContainer />
 
